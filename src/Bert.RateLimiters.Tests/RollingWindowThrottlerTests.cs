b4735b3a86e17a9dc976138a843153116805e158
Robert Mircea <robert.mircea@yahoo.com>
2014-09-20 22:42:12 +0300

    New ratelimiter based on rolling window for controlling exactly the rate of some occurences in a time interval.

---
+++ src/Bert.RateLimiters.Tests/RollingWindowThrottlerTests.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+
+namespace Bert.RateLimiters.Tests
+{
+    public class RollingWindowThrottlerTests
+    {
+        private readonly DateTime referenceTime = new DateTime(2014, 9, 20, 0, 0, 0, DateTimeKind.Utc);
+
+        [Test]
+        public void Throws_WhenNumberOfOccurencesIsLesserThanOne()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() =>new RollingWindowThrottler(-1, TimeSpan.FromSeconds(5)));
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalledWithTokensLessThanOne_WillThrow()
+        {
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            Assert.Throws<ArgumentOutOfRangeException>(() => throttler.ShouldThrottle(0, out waitTimeMillis));
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalled_WillReturnFalse()
+        {
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+
+            shouldThrottle.ShouldBeFalse();
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalledTwiceinSameSecondAndAllows1PerSecond_WillReturnTrue()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.5);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeTrue();
+            waitTimeMillis.ShouldEqual(500);
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.8);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeTrue();
+            waitTimeMillis.ShouldEqual(200);
+        }
+
+
+        [Test]
+        public void ShouldThrottle_WhenCalledAfterSecondPassAndAllows1PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(1, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+        }        
+        
+        [Test]
+        public void ShouldThrottle_WhenCalledTwiceinSameSecondAndAllows2PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.5);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalledAfterSecondPassesAndAllows2PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+            waitTimeMillis.ShouldEqual(0);
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalledThreeTimesinSameSecondAndAllows2PerSecond_WillReturnTrue()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.5);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.7);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeTrue();
+            waitTimeMillis.ShouldEqual(300);
+
+        }
+
+
+        [Test]
+        public void ShouldThrottle_WhenCalledAtEndOfRollingWindowAndAllows2PerSecond_WillReturnFalse()
+        {
+
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            //first rolling window expired, init a new one
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1.2);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            //inside second rolling window, under threshold
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1.3);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            //second rolling window expired, beginning third window
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(2.2);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            //third window, under threshold
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(2.3);
+            shouldThrottle = throttler.ShouldThrottle(1, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalledWithMoreTokensThanOccurrences_WillReturnTrue()
+        {
+            var throttler = new RollingWindowThrottler(2, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.ShouldBeTrue();            
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalledAndConsumingAllTokensAtOnce_WillReturnFalse()
+        {
+            var throttler = new RollingWindowThrottler(3, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+        }
+
+        [Test]
+        public void ShouldThrottle_WhenCalledAndConsumingAllTokensAtOnceAndThenCalledOnceMore_WillReturnTrue()
+        {
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(3, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(0.2);
+            shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.ShouldBeTrue();
+            waitTimeMillis.ShouldEqual(800);
+        }
+
+
+        [Test]
+        public void ShouldThrottle_WhenCalledAndConsumingAllTokensAtOnceAndThenCalledOnceMoreAfterRollingWindowEnd_WillReturnFalse()
+        {
+            SystemTime.SetCurrentTimeUtc = () => referenceTime;
+            var virtualNow = SystemTime.UtcNow;
+
+            var throttler = new RollingWindowThrottler(3, TimeSpan.FromSeconds(1));
+            long waitTimeMillis;
+            var shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+
+            SystemTime.SetCurrentTimeUtc = () => virtualNow.AddSeconds(1.1);
+            shouldThrottle = throttler.ShouldThrottle(3, out waitTimeMillis);
+            shouldThrottle.ShouldBeFalse();
+            waitTimeMillis.ShouldEqual(0);
+        }
+
+    }
+}
---
+++ src/Bert.RateLimiters/RollingWindowThrottler.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bert.RateLimiters
+{
+    /// <summary>
+    /// Used to control the rate of reservations/occurrences per unit of time.
+    /// </summary>
+    /// <remarks>
+    /// The algorithm uses a rolling time window so that precise control over the rate is achieved (without any bursts)
+    /// </remarks>
+    public class RollingWindowThrottler
+    {
+        private readonly int occurrences;
+        private readonly long timeUnitTicks;
+        private readonly object syncRoot = new object();
+        private int remainingTokens;
+        private readonly Queue<long> exitTimesInTicksQueue = new Queue<long>();
+        private long nextCheckTime;
+
+        /// <summary>
+        /// Constructs an instance of the throttler.
+        /// </summary>
+        /// <param name="occurrences">Maximum number of reservation which can be made</param>
+        /// <param name="timeUnit">The time unit in which the reservations can be made</param>
+        public RollingWindowThrottler(int occurrences, TimeSpan timeUnit)
+        {
+            if(occurrences<=0)
+                throw new ArgumentOutOfRangeException("occurrences", "Number of occurences must be a positive integer");
+
+
+            this.occurrences = occurrences;
+            timeUnitTicks = timeUnit.Ticks;
+            remainingTokens = occurrences;
+        }
+
+
+        /// <summary>
+        /// Total number of reservations which can be made for a time unit
+        /// </summary>
+        public int Occurrences
+        {
+            get { return occurrences; }
+        }
+
+        /// <summary>
+        /// The time unit in which the maximal number of <see cref="Occurrences"/> can be reserved.
+        /// </summary>
+        public TimeSpan TimeUnit
+        {
+            get { return TimeSpan.FromTicks(timeUnitTicks); }
+        }
+
+        /// <summary>
+        /// Returns the number of available tokens which can be reserved until the end of current time unit
+        /// </summary>
+        public int AvailableTokens
+        {
+            get
+            {
+                lock (syncRoot)
+                {
+                    return remainingTokens;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Tries to reserve one token in the configured time unit.
+        /// </summary>
+        /// <param name="waitTimeMillis">total suggested wait time till tokens will become available for reservation</param>
+        /// <returns>true if the caller should throttle/wait, or false if reservation was made successfully.</returns>
+        public bool ShouldThrottle(out long waitTimeMillis)
+        {
+            return ShouldThrottle(1, out waitTimeMillis);
+        }
+
+
+        /// <summary>
+        /// Tries to reserve <see cref="tokens"/> in the configured time unit.
+        /// </summary>
+        /// <param name="tokens">total number of reservations</param>
+        /// <param name="waitTimeMillis">total suggested wait time till tokens will become available for reservation</param>
+        /// <returns>true if the caller should throttle/wait, or false if reservation was made successfully.</returns>
+        public bool ShouldThrottle(int tokens, out long waitTimeMillis)
+        {
+            if (tokens <= 0) throw new ArgumentOutOfRangeException("tokens", "Should be positive integer greater than 0");
+            var currentTime = SystemTime.UtcNow.Ticks;
+
+            lock (syncRoot)
+            {
+                CheckExitTimeQueue();
+                if (remainingTokens - tokens >= 0)
+                {
+                    remainingTokens -= tokens;
+                    waitTimeMillis = 0;
+                    long timeToExit = unchecked (currentTime + timeUnitTicks);
+                    for (int i = 0; i < tokens; i++)
+                        exitTimesInTicksQueue.Enqueue(timeToExit);
+                    return false;
+                }
+
+                waitTimeMillis = (nextCheckTime - currentTime) / TimeSpan.TicksPerMillisecond;
+                return true;
+            }
+        }
+
+        public void CheckExitTimeQueue()
+        {
+            if(nextCheckTime > SystemTime.UtcNow.Ticks)
+                return;
+
+            while (exitTimesInTicksQueue.Count > 0 && exitTimesInTicksQueue.Peek() <= SystemTime.UtcNow.Ticks)
+            {
+                exitTimesInTicksQueue.Dequeue();
+                remainingTokens++;
+            }
+
+            //try to determine next check time
+            if (exitTimesInTicksQueue.Count > 0)
+            {
+                var item = exitTimesInTicksQueue.Peek();
+                nextCheckTime = item;
+            }
+            else
+                nextCheckTime = timeUnitTicks;
+        }
+        
+    }
+}
